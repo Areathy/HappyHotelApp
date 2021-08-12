f4a405d52236f8fcf3f7d2c987d6e8a734ab944b
Henrietha <areathy33@gmail.com>
2021-08-12 09:56:33 +0100

    Spy in mockito testing

--- src/test/java/com/mockitotutorial/happyhotel/booking/Test08Spies.java
+++ src/test/java/com/mockitotutorial/happyhotel/booking/Test08Spies.java
@@ -2,13 +2,13 @@ package com.mockitotutorial.happyhotel.booking;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.mockito.ArgumentMatchers.*;
 import java.time.LocalDate;
 
 import static org.mockito.Mockito.*;
 
 
-public class Test07VerifyingBehaviour {
+public class Test08Spies {
+
     private BookingService bookingService;
     private PaymentService paymentServiceMock;
     private RoomService roomServiceMock;
@@ -19,38 +19,40 @@ public class Test07VerifyingBehaviour {
     void setUp() {
         this.paymentServiceMock = mock(PaymentService.class);
         this.roomServiceMock = mock(RoomService.class);
-        this.bookingDAOMock = mock(BookingDAO.class);
+//        this.bookingDAOMock = mock(BookingDAO.class);
+        this.bookingDAOMock = spy(BookingDAO.class);
         this.mailSenderMock = mock(MailSender.class);
 
         this.bookingService = new BookingService(paymentServiceMock, roomServiceMock, bookingDAOMock, mailSenderMock);
     }
 
     @Test
-    void should_InvokePayment_When_Prepaid() {
+    void should_MakeBooking_When_InputOk() {
         //given
         BookingRequest bookingRequest = new BookingRequest("1",
                 LocalDate.of(2021, 01, 01),
                 LocalDate.of(2021, 01, 05), 2, true);
 
         //when
-        bookingService.makeBooking(bookingRequest);
+       String bookingId = bookingService.makeBooking(bookingRequest);
 
         //then
-        verify(paymentServiceMock, times(1)).pay(bookingRequest, 400.0);
-        verifyNoMoreInteractions(paymentServiceMock);
+        verify(bookingDAOMock).save(bookingRequest);
+        System.out.println("Booking Id = " + bookingId);
     }
 
     @Test
-    void should_NotInvokePayment_When_NotPrepaid() {
+    void should_CancelBooking_When_InputOk() {
         //given
         BookingRequest bookingRequest = new BookingRequest("1",
                 LocalDate.of(2021, 01, 01),
-                LocalDate.of(2021, 01, 05), 2, false );
+                LocalDate.of(2021, 01, 05), 2, true);
+        bookingRequest.setRoomId("3");
+        String bookingId = "1";
+
+        doReturn(bookingRequest).when(bookingDAOMock).get(bookingId);
 
         //when
-        bookingService.makeBooking(bookingRequest);
-
-        //then
-        verify(paymentServiceMock, never()).pay(any(), anyDouble());
+        bookingService.cancelBooking(bookingId);
     }
 }
